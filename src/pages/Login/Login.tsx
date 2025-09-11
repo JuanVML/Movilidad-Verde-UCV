b707da2ac0ce104b45fc409c41e870681dbde0aa
Juan VML <jmunozlo01@ucvvirtual.edu.pe>
2025-09-10 20:44:55 -0500

    a

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,15 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import { IonPage, IonContent } from "@ionic/react";
 import "./Login.css";
 
 const Login: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e: React.FormEvent) => {
+    e.preventDefault();
+    alert(`Login con: ${email}`);
+  };
+
   return (
     <IonPage>
-      <IonContent className="login-content">
-        <div className="login-center">
-          <h1>hola</h1>
-          <p>(Login)</p>
-        </div>
+      <IonContent className="login-page" fullscreen>
+        {/* Header con logo */}
+        <header className="login-header">
+         <img
+  src="/assets/logo.png"
+  alt="UCV Green Mobility"
+  className="login-logo"
+/>
+          <h1 className="brand">UCV Green Mobility</h1>
+        </header>
+
+        {/* Card de Login */}
+        <main className="login-card">
+          <h2 className="card-title">Login</h2>
+
+          <form className="login-form" onSubmit={handleLogin}>
+            <input
+              className="input"
+              type="DNI"
+              placeholder="DNI"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+
+            <input
+              className="input"
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+
+            <button className="submit-btn" type="submit">
+              Login
+            </button>
+          </form>
+
+          {/* Divider */}
+          {/*<div className="divider">
+            <span>Or login with</span>
+          </div>
+
+          {/* Social buttons */}
+          {/* <div className="socials">
+            <button className="social-box" aria-label="social-1" />
+            <button className="social-box" aria-label="social-2" />
+          </div>*/}
+
+          <p className="signup">
+            No tienes una cuenta ? <span className="signup-link">Registrate Aqui</span>
+          </p>
+        </main>
       </IonContent>
     </IonPage>
   );
